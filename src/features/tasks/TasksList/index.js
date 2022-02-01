3c96989e70a2c72c7f2d5db160a605d7a249f035
Seweryn Brzozowski <93669333+SewBrzozka@users.noreply.github.com>
2022-02-01 13:47:19 +0100

    action removeTask in react-redux added

--- src/features/tasks/TasksList/index.js
+++ src/features/tasks/TasksList/index.js
@@ -1,8 +1,8 @@
 import { List, Item, Content, Button} from "./styled";
 import { useSelector, useDispatch } from "react-redux";
-import { selectTasks, toggleTaskDone } from "../tasksSlice";
+import { selectTasks, toggleTaskDone, removeTask } from "../tasksSlice";
 
-const TasksList = ({ removeTask }) => {
+const TasksList = () => {
     const { tasks, hideDone } = useSelector(selectTasks);
     const dispatch = useDispatch();
 
@@ -28,7 +28,7 @@ const TasksList = ({ removeTask }) => {
     
                     <Button
                         remove
-                        onClick={() => removeTask(task.id)}
+                        onClick={() => dispatch(removeTask(task.id))}
                     >
                         🗑
                     </Button>
