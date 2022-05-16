c59e5cd2f2ad8ca89815056466a77574eb08963a
Seweryn Brzozowski <93669333+SewBrzozka@users.noreply.github.com>
2022-05-16 12:27:37 +0200

    search bar configuration added

--- src/features/tasks/TasksPage/Search/index.js
+++ src/features/tasks/TasksPage/Search/index.js
@@ -1,15 +1,34 @@
+import { useHistory } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import Input from "../../Input"
+import searchQueryParamName from "../searchQueryParamName";
+import { Wrapper } from "./styled";
 
 const Search = () => {
     const location = useLocation();
-    const query = (new URLSearchParams(location.search)).get("szukaj");
+    const history = useHistory();
+    const query = (new URLSearchParams(location.search)).get(searchQueryParamName);
+
+    const onInputChange = ({ target }) => {
+        const searchParams = new URLSearchParams(location.search);
+
+        if (target.value.trim() === "") {
+            searchParams.delete(searchQueryParamName);
+        } else {
+            searchParams.set(searchQueryParamName, target.value);
+        }
+
+        history.push(`${location.pathname}?${searchParams.toString()}`)
+    };
 
     return (
-        <Input
-            placeholder="filter tasks"
-            value={query || ""}
-        />
+        <Wrapper>
+            <Input
+                placeholder="filter tasks"
+                value={query || ""}
+                onChange={onInputChange}
+            />
+        </Wrapper>
     )
 }
 
