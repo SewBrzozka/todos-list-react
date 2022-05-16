286493eadd3b8d6d4f1086c417b6619c8d1af42d
Seweryn Brzozowski <93669333+SewBrzozka@users.noreply.github.com>
2022-05-16 17:30:22 +0200

    authorPage updated

--- src/features/author/index.js
+++ src/features/author/index.js
@@ -1,6 +1,7 @@
 import Section from "../../common/Section";
 import Header from "../../common/Header";
 import { Container } from "../../common/Container";
+import { ParagraphStyled } from "./styled";
 
 function Authors() {
     return (
@@ -11,7 +12,19 @@ function Authors() {
             <Section
                 title="Seweryn Brzozowski"
                 body={
-                    <> Nazywam się Seweryn Brzozowski  </>
+                    <>
+                        <ParagraphStyled>
+                            <p>
+                                Hello! My name is Seweryn Brzozowski, this year I am turning 30.📅😱<br />
+                                I decided to change something in my life and I am trying my hand at programming. 💡📈👨‍💻
+                            </p>
+                            <p>
+                                I have been interested in sports all my life. I have been training fencing for 19 years.
+                                I am a member of the national team and represent Poland in international arenas. 🏅🤺
+                            </p>
+
+                        </ParagraphStyled>
+                    </>
                 }
             />
         </Container>
---
+++ src/features/author/styled.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const ParagraphStyled = styled.div`
+    font-family: 'Lato', sans-serif;
+    line-height: 2.5;
+    text-align: left;
+    margin:10px;
+    margin-top: 0px;
+    padding: 10px;
+`;
